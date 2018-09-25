9e04f01fc111f52067ad3fc88e4dc40a2cd3cd31
Paul Sonkoly <sonkoly.pal@gmail.com>
2018-09-25 09:47:10 +0100

    FloatParser example

--- README.md
+++ README.md
@@ -29,3 +29,43 @@ end
 counter = Counter.new
 counter.run_mealy([1,1,1,1,0,1,0,0]).first # => 4
 ```
+
+### Float parser
+
+This example could be simplified with regexps, but we demonstrate how to
+parse floats with single character based rules. One could also use regexps to
+match tokens in their FSM rules.
+
+```ruby
+class FloatParser
+  include Mealy::DSL
+
+  initial_state(:first)
+
+  10.times { |ix| transition(from: :first, to: :before_dot, on: ix.to_s ) }
+
+  10.times { |ix| read(state: :before_dot, on: ix.to_s) }
+
+  transition(from: :before_dot, to: :after_dot, on: ?.)
+
+  10.times { |ix| read(state: :after_dot, on: ix.to_s) }
+
+  transition(from: [ :first, :before_dot, :after_dot ], to: :error) do |c, from|
+    @error = "unexpected char #{c} @ #{from.inspect}"
+  end
+
+  read(state: :error)
+
+  attr_reader :error
+end
+
+p = FloatParser.new
+p.run_mealy('1'.chars) {}
+p.error # => nil
+p.run_mealy('1.0'.chars) {}
+p.error # => nil
+p.run_mealy('.0'.chars) {}
+p.error # => "unexpected char . @ :first"
+p.run_mealy('1.2.0'.chars) {}
+p.error # => "unexpected char . @ :after_dot"
+```
--- lib/mealy.rb
+++ lib/mealy.rb
@@ -32,6 +32,45 @@
 #     counter = Counter.new
 #     counter.run_mealy([1,1,1,1,0,1,0,0]).first # => 4
 #
+# == Float parser
+#
+# This example could be simplified with regexps, but we demonstrate how to
+# parse floats with single character based rules. One could also use regexps to
+# match tokens in their FSM rules.
+#
+#
+#     class FloatParser
+#       include Mealy::DSL
+#
+#       initial_state(:first)
+#
+#       10.times { |ix| transition(from: :first, to: :before_dot, on: ix.to_s ) }
+#
+#       10.times { |ix| read(state: :before_dot, on: ix.to_s) }
+#
+#       transition(from: :before_dot, to: :after_dot, on: ?.)
+#
+#       10.times { |ix| read(state: :after_dot, on: ix.to_s) }
+#
+#       transition(from: [ :first, :before_dot, :after_dot ], to: :error) do |c, from|
+#         @error = "unexpected char #{c} @ #{from.inspect}"
+#       end
+#
+#       read(state: :error)
+#
+#       attr_reader :error
+#     end
+#
+#     p = FloatParser.new
+#     p.run_mealy('1'.chars) {}
+#     p.error # => nil
+#     p.run_mealy('1.0'.chars) {}
+#     p.error # => nil
+#     p.run_mealy('.0'.chars) {}
+#     p.error # => "unexpected char . @ :first"
+#     p.run_mealy('1.2.0'.chars) {}
+#     p.error # => "unexpected char . @ :after_dot"
+#
 # @note states can be represented with any type not just Symbols
 module Mealy
   # Error indicating that there is no transition from the current state with
@@ -118,7 +157,7 @@ module Mealy
 
     %i[init transitions finish].each do |sym|
       define_method(sym) do
-        self.class.instance_variable_get(:"@#{sym}")
+        self.class.instance_variable_get(:"@#{sym}") # !> instance variable @finish not initialized
       end
     end
 
